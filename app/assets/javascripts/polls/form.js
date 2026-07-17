145c543869a3c8b920a54ab01951a6af078424ec
taitus <sebastia.roig@gmail.com>
2026-07-17 21:01:42 +0200

    Allow submitting custom text for poll options
    Enable users to provide custom text when voting for poll options
    that allow custom text. This is useful for "Other (please specify)"
    type answers where predefined options are not sufficient.
    
    Note: We use text_area instead of text_field to better accommodate
    the 1000 character limit, even though it displays as a single row.

--- app/assets/javascripts/polls/form.js
+++ app/assets/javascripts/polls/form.js
@@ -1,6 +1,12 @@
 (function() {
   "use strict";
   App.PollsForm = {
+    syncOpenText: function(input) {
+      var $input = $(input);
+      var $textarea = $(".custom-answer[data-selects='" + $input.attr("id") + "']");
+
+      $textarea.prop("disabled", !$input.prop("checked"));
+    },
     updateMultipleChoiceStatus: function(fieldset) {
       var max_votes = $(fieldset).attr("data-max-votes");
       var checked_boxes = $(fieldset).find(":checkbox:checked");
@@ -11,16 +17,32 @@
       } else {
         $(fieldset).find(":checkbox").prop("disabled", false);
       }
+
+      $(fieldset).find(":checkbox").each(function() {
+        App.PollsForm.syncOpenText(this);
+      });
     },
+    updateRadioChoiceStatus: function(fieldset) {
+      $(fieldset).find(":radio").each(function() {
+        App.PollsForm.syncOpenText(this);
+      });
+    },
+
     initialize: function() {
       $(".poll-form .multiple-choice").each(function() {
         App.PollsForm.updateMultipleChoiceStatus(this);
       });
 
       $(".poll-form .multiple-choice :checkbox").on("change", function() {
-        var fieldset = $(this).closest("fieldset");
+        App.PollsForm.updateMultipleChoiceStatus($(this).closest("fieldset")[0]);
+      });
 
-        App.PollsForm.updateMultipleChoiceStatus(fieldset);
+      $(".poll-form .single-choice").each(function() {
+        App.PollsForm.updateRadioChoiceStatus(this);
+      });
+
+      $(".poll-form .single-choice :radio").on("change", function() {
+        App.PollsForm.updateRadioChoiceStatus($(this).closest("fieldset")[0]);
       });
     }
   };
